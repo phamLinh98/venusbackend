d03832384c2c2ef431854fefa77d8e97ee2e03c5
“linhPham98” <linhthusinh98@gmail>
2024-09-19 23:13:39 +0900

    config db mock

---
+++ server.js
@@ -0,0 +1,21 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
+server.listen(4000, () => {
+  console.log("DB run successfully");
+});
+
+// Export the Server API
+module.exports = server;
