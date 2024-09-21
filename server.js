473bf5cd7b9bd54d65933b1aa06ee81a6cb7366f
“linhPham98” <linhthusinh98@gmail>
2024-09-22 01:59:33 +0900

    commit cors

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
+// Restrict CORS to only allow requests from https://linhthusinh.vercel.app
+const corsOptions = {
+  origin: "https://linhthusinh.vercel.app",
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+
+server.use(cors(corsOptions)); // Apply the CORS middleware
 server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
@@ -12,9 +21,11 @@ server.use(
     "/api/*": "/$1",
   })
 );
+
 server.use(router);
+
 server.listen(4000, () => {
-  console.log("Backend Node Sever Run Success");
+  console.log("Backend Node Server Run Success");
 });
 
 // Export the Server API
